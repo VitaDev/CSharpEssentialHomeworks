81743664339dad1e3bd1fb74d633c7201694047d
VitaDev <40732826+VitaDev@users.noreply.github.com>
2021-07-01 06:33:05 +0300

    Created new project

---
+++ Essential_homeworks/Essential_homeworks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Essential_homeworks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
