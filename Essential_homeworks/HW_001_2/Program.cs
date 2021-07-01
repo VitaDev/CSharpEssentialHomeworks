8dd8add7cb5bab229ff73888c98160427533a8cc
VitaDev <40732826+VitaDev@users.noreply.github.com>
2021-07-01 07:50:31 +0300

    Added HW_001_2

---
+++ Essential_homeworks/HW_001_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HW_001_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину сторон прямоугольника");
+            Console.Write("Сторона 1: ");
+            double side1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Сторона 2: ");
+            double side2 = Convert.ToDouble(Console.ReadLine());
+            Rectangle rectangle = new Rectangle(side1, side2);
+            Console.WriteLine($"Площадь прямоугольника: {rectangle.AreaCalculator()}");
+            Console.WriteLine($"Периметр прямоугольника: {rectangle.PerimeterCalculator()}");
+        }
+    }
+}
---
+++ Essential_homeworks/HW_001_2/Rectangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_001_2
+{
+    public class Rectangle
+    {
+        public Rectangle(double side1, double side2)
+        {
+            this.Side1 = side1;
+            this.Side2 = side2;
+        }
+        public double Side1 { get; set; }
+        public double Side2 { get; set; }
+
+        public double AreaCalculator()
+        {
+            return this.Side1 * this.Side2;
+        }
+        public double PerimeterCalculator()
+        {
+            return (this.Side1 + this.Side2) * 2 ;
+        }
+    }
+}
