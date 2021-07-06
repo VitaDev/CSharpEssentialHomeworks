1f2458b829c578253212f7ca29745dd1a89c2ae7
VitaDev <40732826+VitaDev@users.noreply.github.com>
2021-07-06 15:34:34 +0300

    HW_005_2

---
+++ Essential_homeworks/HW_005_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace HW_005_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину массива: ");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            int[] mas = new int[amount];
+            Console.WriteLine("Заполните массив");
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine($"Минимальное значение: {mas.Min()}"); 
+            Console.WriteLine($"Максимальное значение: {mas.Max()}");
+            Console.WriteLine($"Сумма: {mas.Sum()}");
+            Console.WriteLine($"Среднее арифметическое: {mas.Sum()/amount}");
+            Console.WriteLine("Нечётные значения:");
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (mas[i] % 2 != 0)
+                {
+                    Console.WriteLine(mas[i]);
+                }
+            }
+            Console.WriteLine(new String('-', 50));
+        }
+    }
+}
