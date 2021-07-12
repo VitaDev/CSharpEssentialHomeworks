0b557ad4f35bb365c51ab77a9213370c6811fa6e
VitaDev <40732826+VitaDev@users.noreply.github.com>
2021-07-12 14:51:03 +0300

    HW_009_3

---
+++ Essential_homeworks/HW_009_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HW_009_3
+{
+    class Program
+    {
+        delegate int MyDelegate();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Среднее арифметическое значений делегатов");
+            Random random = new Random();
+            MyDelegate myDelegate = () => { return random.Next(1, 100); };
+            myDelegate += () => { return random.Next(100, 1000); };
+            myDelegate += () => { return random.Next(1000, 10000); };
+            MyDelegate[] myDelegates = new MyDelegate[] { myDelegate, myDelegate, myDelegate };
+            int sum = 0;
+            int count = 0;
+            foreach (var item in myDelegates)
+            {
+                sum += item();
+                Delegate[] delegates = item.GetInvocationList();
+                count += delegates.Length;
+            }
+            Console.WriteLine($"Среднее арифметическое: {Math.Round((double)sum/count, 1)}");
+        }
+    }
+}
